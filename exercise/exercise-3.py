a2488ef782a04a74c927a21c459c167e37026638
Zeynp62 <zainab.fadhel.alaali62@gmail.com>
2025-01-06 22:32:34 +0300

    Q3 done

--- exercise/exercise-3.py
+++ exercise/exercise-3.py
@@ -10,3 +10,15 @@
 #      The dog's age in dog years is xx
 
 # Hint:  Use the int() function to convert the string returned from input() into an integer
+
+def to_dog_age (a):
+    if a<3:
+        x=a*10
+        print(f"The dog's age in dog years is {x}")
+    else:
+        x=20+((a-2)*7)
+        print(f"The dog's age in dog years is {x}")
+
+
+age = input("Input a dog's age in human years:")
+to_dog_age(int(age))
